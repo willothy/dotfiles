cdd04cfd5ad8d254bee051a8fe13bf2a21caed49
Will Hopkins <willothyh@gmail.com>
2023-04-15 16:54:21 -0700

    fix: gitdir freeze - will write gitdir func in Rust

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -75,7 +75,7 @@ local function setup(cfg)
 	}
 end
 
-local function popup(title, message, opts)
+local function _popup(title, message, opts)
 	opts = opts or {}
 	if not opts.timeout_ms then
 		opts.timeout_ms = 3000
@@ -112,7 +112,8 @@ local process_icons = {
 		Text = wezterm.nerdfonts.mdi_hexagon,
 	},
 	["zsh"] = {
-		Text = wezterm.nerdfonts.dev_terminal,
+		Text = wezterm.nerdfonts.dev_terminal_badge,
+		-- Text = wezterm.nerdfonts.mdi_apple_keyboard_command,
 	},
 	["bash"] = {
 		Text = wezterm.nerdfonts.cod_terminal_bash,
@@ -124,6 +125,9 @@ local process_icons = {
 		Text = wezterm.nerdfonts.mdi_chart_donut_variant,
 	},
 	["cargo"] = {
+		Text = wezterm.nerdfonts.dev_rust,
+	},
+	["rust"] = {
 		Text = wezterm.nerdfonts.dev_rust,
 	},
 	["go"] = {
@@ -149,32 +153,71 @@ local process_icons = {
 	},
 }
 
+local function exists(file)
+	local f = io.open(file)
+	return f and io.close(f)
+end
+
+-- Lua implementation of PHP scandir function
+function scandir(directory)
+	local i, t, popen = 0, {}, io.popen
+	local pfile = popen('ls -a "' .. directory .. '"')
+	for filename in pfile:lines() do
+		i = i + 1
+		t[i] = filename
+	end
+	pfile:close()
+	return t
+end
+
+local function gitcheck(directory)
+	local d = directory
+	if not exists(d) then
+		return nil
+	end
+	while #d > 1 and not exists(d .. "/.git") do
+		d = d:gsub("/+[^/]*$", "")
+	end
+	return #d > 0 and d or directory
+end
+
 ---Returns the dir relative to git root, or the original dir
-local function get_git_root(dir)
-	local function exists(file)
-		local f = io.open(file)
-		return f and io.close(f)
-	end
-
-	local function gitcheck(directory)
-		local d = directory
-		while #d > 0 and not exists(d .. "/.git") do
-			d = d:gsub("/+[^/]*$", "")
-		end
-		return #d > 0 and d or directory
-	end
-
+local function get_git_root(dir, truncate_base)
 	local gc = gitcheck(dir)
-	if gc == dir then
+	if not gc then
 		return dir
+	end
+	if gc == dir then
+		if truncate_base == false then
+			return dir
+		else
+			return dir:match(".*/([^/]+)[/]?$")
+		end
 	else
-		return dir:gsub(gc:gsub("%w+$", ""), ""):gsub("/$", "")
+		local base = gc:gsub("%w+[/]?$", "")
+		if base == "" then
+			return dir
+		end
+		return dir:gsub(base, "")
 	end
 end
 
+local HOME = wezterm.home_dir
+
+local pane_cwd_cache = {}
+
 local function get_current_working_dir(tab, max)
-	local pwd = tab.active_pane.current_working_dir:gsub("%w+://%w+/", "/")
-	pwd = get_git_root(pwd):gsub(os.getenv("HOME"), "~")
+	local pane = tab.active_pane
+	local pwd = ""
+
+	if pane_cwd_cache[pane.pane_id] ~= nil then
+		pwd = pane_cwd_cache[pane.pane_id]
+	else
+		pwd = pane.current_working_dir:gsub("%w+://%w+/", "/")
+	end
+
+	-- pwd = get_git_root(pwd, false)
+	pwd = pwd:gsub(HOME, "~")
 	if pwd == "~/" then
 		return "~"
 	end
@@ -224,21 +267,113 @@ local function get_process(tab)
 	end
 end
 
-wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+wezterm.on("open-uri", function(_window, _pane, uri)
+	wezterm.open_with(uri, "chromium")
+end)
+
+wezterm.on("format-tab-title", function(tab, _tabs, _panes, _config, hover, max_width)
+	local has_unseen_output = false
+	for _, pane in ipairs(tab.panes) do
+		if pane.has_unseen_output then
+			has_unseen_output = true
+			break
+		end
+	end
 	return {
+		hover and {
+			Background = { Color = "#2a2e36" },
+		} or "ResetAttributes",
+		{
+			Foreground = {
+				Color = has_unseen_output and "#fffac2" or (tab.is_active and "#5de4c7" or "#9196c2"),
+			},
+		},
 		{ Text = get_process(tab) },
+		{ Foreground = { Color = "#9196c2" } },
 		{ Text = " " },
-		{ Text = get_current_working_dir(tab, 14) },
+		-- { Text = tab.active_pane.current_working_dir },
+		{ Text = get_current_working_dir(tab, math.min(max_width, 14)) },
 		{ Text = " " },
 	}
 end)
 
-wezterm.on("update-right-status", function(window)
+local function find(list, item)
+	for i, v in ipairs(list) do
+		if type(item) == "function" then
+			if item(v) == true then
+				return i
+			end
+		else
+			if v == item then
+				return i
+			end
+		end
+	end
+	return nil
+end
+
+wezterm.on("update-right-status", function(window, pane)
 	-- if not window:get_dimensions().is_full_screen then
 	-- 	window:set_right_status("")
 	-- 	return
 	-- end
+	-- local pwd = ""
+	-- if pane ~= nil then
+	-- 	local panewd = pane:get_current_working_dir()
+	-- 	if panewd and panewd ~= "" then
+	-- 		pwd = get_git_root(panewd:gsub("%w+://%w+/", "/"), false)
+	-- 	end
+	-- end
 
+	local icon = "✦"
+	-- if pwd ~= "" and exists(pwd) then
+	-- 	local entries = scandir(pwd .. "/")
+	-- 	if exists(pwd .. "/src/") then
+	-- 		for _, v in ipairs(scandir(pwd .. "/src/")) do
+	-- 			table.insert(entries, v)
+	-- 		end
+	-- 	end
+	--
+	-- 	-- local lua = exists(pwd .. "/init.lua") or exists(pwd .. "/wezterm.lua")
+	-- 	if
+	-- 		exists(pwd .. "/Cargo.toml")
+	-- 		or find(entries, function(v)
+	-- 				if string.sub(v, -2, -1) == "rs" then
+	-- 					return true
+	-- 				end
+	-- 				return false
+	-- 			end)
+	-- 			~= nil
+	-- 	then
+	-- 		icon = wezterm.nerdfonts.dev_rust
+	-- 	elseif
+	-- 		exists(pwd .. "/init.lua")
+	-- 		or find(entries, function(v)
+	-- 				if string.sub(v, -3, -1) == "lua" then
+	-- 					return true
+	-- 				end
+	-- 				return false
+	-- 			end)
+	-- 			~= nil
+	-- 	then
+	-- 		icon = wezterm.nerdfonts.seti_lua
+	-- 	elseif
+	-- 		exists(pwd .. "/package.json")
+	-- 		or exists(pwd .. "/node_modules")
+	-- 		or find(entries, function(v)
+	-- 				if string.sub(v, -3, -1) == "js" then
+	-- 					return true
+	-- 				end
+	-- 				return false
+	-- 			end)
+	-- 			~= nil
+	-- 	then
+	-- 		icon = wezterm.nerdfonts.dev_javascript_badge
+	-- 	elseif exists(pwd .. "/go.mod") then
+	-- 		icon = wezterm.nerdfonts.mdi_language_go
+	-- 	end
+	-- end
+	--
 	window:set_right_status(wezterm.format({
 		-- Pane info
 		-- { Foreground = { Color = "#9196c2" } },
@@ -246,6 +381,7 @@ wezterm.on("update-right-status", function(window)
 		-- Time
 		{ Attribute = { Intensity = "Bold" } },
 		{ Foreground = { Color = "#9196c2" } },
+		{ Text = icon or "" },
 		{ Text = wezterm.strftime(" %l:%M %p ") },
 	}))
 end)
