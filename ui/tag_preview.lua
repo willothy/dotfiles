f221ba31688ebb064fe4fae282641c696b7006bf
Will Hopkins <willothyh@gmail.com>
2023-06-30 11:43:05 -0700

    Squashed '.config/awesome/' content from commit 685d091
    git-subtree-dir: .config/awesome
    git-subtree-split: 685d091b5b5273e19909cd5d307e9f6b5a3b7b22

---
+++ ui/tag_preview.lua
@@ -0,0 +1,25 @@
+local bling = require("bling")
+local awful = require("awful")
+local wibox = require("wibox")
+local beautiful = require("beautiful")
+
+bling.widget.tag_preview.enable({
+	show_client_content = false,
+	scale = 0.25,
+	honor_padding = false,
+	honor_workarea = false,
+	placement_fn = function(c)
+		awful.placement.top_left(c, {
+			margins = {
+				top = beautiful.bar_height + beautiful.useless_gap * 2,
+				left = beautiful.useless_gap * 2,
+			},
+		})
+	end,
+	background_widget = wibox.widget({
+		image = beautiful.wallpaper,
+		horizontal_fit_policy = "fit",
+		vertical_fit_policy = "fit",
+		widget = wibox.widget.imagebox,
+	}),
+})
