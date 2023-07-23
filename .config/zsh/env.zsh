51e219dac6626143a9d60fae2da663eba88e5db3
Will Hopkins <willothyh@gmail.com>
2023-07-23 14:13:43 -0700

    updates

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -6,7 +6,7 @@ export PATH="${HOME}/go/bin:${PATH}"
 export PATH="${HOME}/.luarocks/bin:${PATH}"
 export PATH="${HOME}/vendor/zig:${PATH}"
 
-export WORDCHARS='-_. '
+export WORDCHARS='-_.'
 export LS_COLORS=$LS_COLORS:'di=1;34:'
 
 export VISUAL="nvim -b"
