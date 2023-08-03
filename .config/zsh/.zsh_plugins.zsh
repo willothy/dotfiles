2fa2b93c89921a1c1ee076487269996440f1ce18
Will Hopkins <willothyh@gmail.com>
2023-08-02 23:16:17 -0700

    updates

--- .config/zsh/.zsh_plugins.zsh
+++ .config/zsh/.zsh_plugins.zsh
@@ -2,12 +2,12 @@ fpath+=( /home/willothy/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLA
 source /home/willothy/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
 fpath+=( /home/willothy/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting )
 source /home/willothy/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
-fpath+=( /home/willothy/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-mroth-SLASH-evalcache )
-source /home/willothy/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-mroth-SLASH-evalcache/evalcache.plugin.zsh
 fpath+=( /home/willothy/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ael-code-SLASH-zsh-colored-man-pages )
 source /home/willothy/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ael-code-SLASH-zsh-colored-man-pages/colored-man-pages.plugin.zsh
 fpath+=( /home/willothy/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-jeffreytse-SLASH-zsh-vi-mode )
 source /home/willothy/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-jeffreytse-SLASH-zsh-vi-mode/zsh-vi-mode.plugin.zsh
+fpath+=( /home/willothy/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-mroth-SLASH-evalcache )
+source /home/willothy/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-mroth-SLASH-evalcache/evalcache.plugin.zsh
 export PATH="/home/willothy/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-bigH-SLASH-git-fuzzy/bin:$PATH"
 export PATH="/home/willothy/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-tj-SLASH-git-extras/bin:$PATH"
 fpath+=( /home/willothy/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-tj-SLASH-git-extras/etc )
