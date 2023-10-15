7856a2f09d70b0299dd130d0483dc5b5c3aac6be
Will Hopkins <willothyh@gmail.com>
2023-10-15 16:38:35 -0700

    update wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -464,9 +464,7 @@ wezterm.on("format-tab-title", function(tab, _tabs, _panes, _config, hover, _max
 		},
 		{ Text = icon },
 		{ Foreground = { Color = "#9196c2" } },
-		{ Text = title },
-		-- { Text = get_current_working_dir(tab, 1) },
-		-- { Text = " " },
+		{ Text = tab.tab_title == "" and title or tab.tab_title },
 	}
 end)
 
