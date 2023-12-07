d6dc3c73d25ed9943d5346141d7b2d938dd393d6
Will Hopkins <willothyh@gmail.com>
2023-12-06 19:05:48 -0800

    update wezterm and nvim configs

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -617,10 +617,6 @@ end)
 -- end)
 
 config.hyperlink_rules = {
-	-- Linkify things that look like URLs and the host has a TLD name.
-	-- Compiled-in default. Used if you don't specify any hyperlink_rules.
-	{ regex = "\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b", format = "$0" },
-
 	-- linkify email addresses
 	-- Compiled-in default. Used if you don't specify any hyperlink_rules.
 	{ regex = [[\b\w+@[\w-]+(\.[\w-]+)+\b]], format = "mailto:$0" },
@@ -633,6 +629,8 @@ config.hyperlink_rules = {
 	-- As long as a full URL hyperlink regex exists above this it should not match a full URL to
 	-- GitHub or GitLab / BitBucket (i.e. https://gitlab.com/user/project.git is still a whole clickable URL)
 	{ regex = [["([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)"]], format = "https://www.github.com/$1/$3" },
+
+	{ regex = [[localhost:\d+]], format = "http://localhost:$1" },
 }
 
 config.warn_about_missing_glyphs = false
