901931877d688861135d7234706cd5bbf744f533
Will Hopkins <willothyh@gmail.com>
2024-02-06 22:31:19 -0800

    add picom.lua

---
+++ .config/picom/picom.lua
@@ -0,0 +1,36 @@
+---@diagnostic disable: lowercase-global
+
+backend = "egl"
+vsync = true
+detect_transient = false
+
+animations = true
+animation_stiffness = 100
+animation_stiffness_tag_change = 90
+
+animation_window_mass = 0.45
+animation_dampening = 10
+animation_clamping = true
+
+active_opacity = 1
+inactive_opacity = 1
+inactive_opacity_override = false
+frame_opacity = 1
+detect_client_opacity = true
+
+corner_radius = 12
+detect_rounded_corners = false
+
+blur_method = "dual_kawase"
+blur_strength = 5
+blur_radius = 5
+blur_background_frame = true
+
+animation_for_open_window = "zoom"
+animation_for_unmap_window = "zoom"
+animation_for_transient_window = "slide-up"
+
+animation_for_prev_tag = "slide-up"
+animation_for_next_tag = "zoom"
+enable_fading_next_tag = true
+enable_fading_prev_tag = true
