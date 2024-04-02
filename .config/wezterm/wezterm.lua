a372364b3b0b043a1507e2fdbcd3300e0f929179
Will Hopkins <willothyh@gmail.com>
2024-04-01 22:47:04 -0700

    updates

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -205,21 +205,66 @@ end)
 -- end)
 
 config.hyperlink_rules = {
-	-- linkify email addresses
-	-- Compiled-in default. Used if you don't specify any hyperlink_rules.
+	-- Matches: a URL in parens: (URL)
+	{
+		regex = "\\((\\w+://\\S+)\\)",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Matches: a URL in brackets: [URL]
+	{
+		regex = "\\[(\\w+://\\S+)\\]",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Matches: a URL in curly braces: {URL}
+	{
+		regex = "\\{(\\w+://\\S+)\\}",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Matches: a URL in angle brackets: <URL>
+	{
+		regex = "<(\\w+://\\S+)>",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Then handle URLs not wrapped in brackets
+	{
+		regex = "\\b\\w+://\\S+[)/a-zA-Z0-9-]+",
+		format = "$0",
+	},
+	-- implicit mailto link
+	{
+		regex = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
+		format = "mailto:$0",
+	},
 	{ regex = [[\b\w+@[\w-]+(\.[\w-]+)+\b]], format = "mailto:$0" },
 	-- file:// URI
 	-- Compiled-in default. Used if you don't specify any hyperlink_rules.
 	{ regex = [[\bfile://\S*\b]], format = "$0" },
-
-	-- Make username/project paths clickable. This implies paths like the following are for GitHub.
-	-- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wez/wezterm | "wez/wezterm.git" )
-	-- As long as a full URL hyperlink regex exists above this it should not match a full URL to
-	-- GitHub or GitLab / BitBucket (i.e. https://gitlab.com/user/project.git is still a whole clickable URL)
-	{ regex = [["([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)"]], format = "https://www.github.com/$1/$3" },
-
-	-- { regex = [[localhost:(\d+)]], format = "http://localhost:$1" },
 }
+
+-- make username/project paths clickable. this implies paths like the following are for github.
+-- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wez/wezterm | "wez/wezterm.git" )
+-- as long as a full url hyperlink regex exists above this it should not match a full url to
+-- github or gitlab / bitbucket (i.e. https://gitlab.com/user/project.git is still a whole clickable url)
+table.insert(config.hyperlink_rules, {
+	regex = [[[^:]["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+	format = "https://www.github.com/$1/$3",
+})
+
+-- localhost, with protocol, with optional port and path
+table.insert(config.hyperlink_rules, {
+	regex = [[http(s)?://localhost(?>:\d+)?]],
+	format = "http$1://localhost:$2",
+})
+
+-- localhost with no protocol, with optional port and path
+table.insert(config.hyperlink_rules, {
+	regex = [[[^/]localhost:(\d+)(\/\w+)*]],
+	format = "http://localhost:$1",
+})
 
 config.warn_about_missing_glyphs = false
 
